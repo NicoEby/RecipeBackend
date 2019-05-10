e74117c8ae5e2480657a0de9d10a6d3b28582928
lussil <lussil@TROMW033603>
2019-05-10 09:16:43 +0200

    initial

---
+++ recipe.data/Models/RecipeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recipe.data.Models
+{
+    public class RecipeContext
+    {
+        //public static List<Recipe> Recipes = new List<Recipe>();
+        //public RecipeContext()
+        //{
+
+        //}
+    }
+}
